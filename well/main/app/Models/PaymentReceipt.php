b71af07cb213db48cc41c3248b2477c0188d3207
MD-Jobair677 <mdjobairhossain618@gmail.com>
2026-01-15 21:27:06 +0600

    add cattle name in booking payment whene add payment

---
+++ well/main/app/Models/PaymentReceipt.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PaymentReceipt extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+
+        'booking_id',
+        'payment_uid',
+        'receipt_tk',
+        'comment',
+        'printed_at',
+    ];
+
+
+
+    function booking(){
+        return $this->belongsTo(Booking::class,'booking_id');
+    }
+}
